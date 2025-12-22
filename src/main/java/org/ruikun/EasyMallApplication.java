66a5e256cb3a62d36538c4ad4ccd59899175f9e2
yunluoxincheng <yunluoxincheng@outlook.com>
2025-12-22 17:02:54 +0800

    easymall-v0.0.1

--- src/main/java/org/ruikun/EasyMallApplication.java
+++ src/main/java/org/ruikun/EasyMallApplication.java
@@ -1,9 +1,11 @@
 package org.ruikun;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.ruikun.mapper")
 public class EasyMallApplication {
 
     public static void main(String[] args) {
